d675234e027cf93f833ad376f98a8a10e8d8f649
digs130502 <113381944+digs130502@users.noreply.github.com>
2025-02-12 11:53:38 -0600

    started game project 4

---
+++ weeklyproject4/main.go
@@ -0,0 +1,35 @@
+package main
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+func main() {
+	rl.InitWindow(800, 450, "Flappy Bird")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	var playerY float32 = 200
+	var speed float32 = 100
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.RayWhite)
+		rl.DrawRectangle(100, int32(playerY), 50, 50, rl.Orange)
+		rl.DrawText("Points: ", 5, 5, 20, rl.Black)
+
+		if rl.IsKeyDown(rl.KeyW) && playerY > 0 {
+			playerY -= speed * rl.GetFrameTime()
+		}
+		if rl.IsKeyDown(rl.KeyS) && playerY+50 < 450 {
+			playerY += speed * rl.GetFrameTime()
+		}
+
+		if rl.IsKeyDown(rl.KeyR) {
+			break
+		}
+
+		rl.EndDrawing()
+	}
+
+}
