419d3fefffc80f776b94e0ae9e4cb45c01da7c87
digs130502 <113381944+digs130502@users.noreply.github.com>
2025-02-15 13:35:21 -0600

    continued weekly project 4

--- weeklyproject4/main.go
+++ weeklyproject4/main.go
@@ -10,13 +10,18 @@ func main() {
 
 	var playerY float32 = 200
 	var speed float32 = 100
+	var pipeX float32 = 750
+	var pipeSpeed float32 = 100
 
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
 		rl.DrawRectangle(100, int32(playerY), 50, 50, rl.Orange)
+		rl.DrawRectangle(int32(pipeX), 0, 70, 450, rl.Green)
 		rl.DrawText("Points: ", 5, 5, 20, rl.Black)
+
+		pipeX -= pipeSpeed * rl.GetFrameTime()
 
 		if rl.IsKeyDown(rl.KeyW) && playerY > 0 {
 			playerY -= speed * rl.GetFrameTime()
@@ -27,6 +32,10 @@ func main() {
 
 		if rl.IsKeyDown(rl.KeyR) {
 			break
+		}
+
+		if pipeX <= -50 {
+			pipeX = 750
 		}
 
 		rl.EndDrawing()
