06664fb3e46aa724b8bdf75afb350beb6cc6dff7
digs130502 <113381944+digs130502@users.noreply.github.com>
2025-03-04 11:19:52 -0600

    finished micro12

--- micro12/main.go
+++ micro12/main.go
@@ -8,8 +8,45 @@ func main() {
 	rl.InitWindow(800, 450, "Micro Assignment 12")
 	defer rl.CloseWindow()
 
-	if !rl.WindowShouldClose() {
+	rl.SetTargetFPS(60)
+
+	rl.InitAudioDevice()
+
+	birds := rl.LoadSound("audio/birds.wav")
+	piano := rl.LoadSound("audio/piano.wav")
+	woodpecker := rl.LoadSound("audio/woodpecker.wav")
+	whoosh := rl.LoadSound("audio/whoosh.wav")
+	music := rl.LoadSound("audio/music.wav")
+
+	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
+
+		rl.ClearBackground(rl.Black)
+
+		rl.DrawRectangle(275, 200, 50, 50, rl.Orange)
+		rl.DrawRectangle(335, 200, 50, 50, rl.Orange)
+		rl.DrawRectangle(395, 200, 50, 50, rl.Orange)
+		rl.DrawRectangle(455, 200, 50, 50, rl.Orange)
+		rl.DrawRectangle(515, 200, 50, 50, rl.Orange)
+
+		rl.DrawText("Press keys 1-5 for sounds!", 275, 300, 20, rl.RayWhite)
+
+		if rl.IsKeyPressed(rl.KeyOne) {
+			rl.PlaySound(birds)
+		}
+		if rl.IsKeyPressed(rl.KeyTwo) {
+			rl.PlaySound(piano)
+		}
+		if rl.IsKeyPressed(rl.KeyThree) {
+			rl.PlaySound(woodpecker)
+		}
+		if rl.IsKeyPressed(rl.KeyFour) {
+			rl.PlaySound(whoosh)
+		}
+		if rl.IsKeyPressed(rl.KeyFive) {
+			rl.PlaySound(music)
+		}
+
 		rl.EndDrawing()
 	}
 }
