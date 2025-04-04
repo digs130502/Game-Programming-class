9606bf69faeaebfa2ae9e6a87ebc15be119a870b
digs130502 <113381944+digs130502@users.noreply.github.com>
2025-04-04 11:52:39 -0500

    started project 9

---
+++ weeklyproject9/main.go
@@ -0,0 +1,16 @@
+package main
+
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+func main() {
+	rl.InitWindow(800, 450, "Fighting Game")
+	defer rl.CloseWindow()
+
+	rl.SetTargetFPS(60)
+
+	for !rl.WindowShouldClose() {
+		rl.BeginDrawing()
+		rl.ClearBackground(rl.RayWhite)
+		rl.EndDrawing()
+	}
+}
