3530c98c7669e4196db610b5b9d3e7bd2751ed2a
digs130502 <113381944+digs130502@users.noreply.github.com>
2025-04-08 13:09:28 -0500

    finished all logic only need animations and special attack left

--- weeklyproject10/main.go
+++ weeklyproject10/main.go
@@ -10,6 +10,8 @@ func main() {
 
 	rl.SetTargetFPS(60)
 
+	isGameOver := false
+
 	pl1 := NewPlayer1()
 	pl2 := NewPlayer2()
 	h1 := NewHealthBar1(&pl1)
@@ -17,20 +19,49 @@ func main() {
 
 	for !rl.WindowShouldClose() {
 
-		//Updates
-		UpdatePlayers(&pl1, &pl2)
-		CheckPlayerFloorCollisions(&pl1, &pl2)
-		CheckMovement(&pl1, &pl2)
-		CheckDamage(&pl1, &pl2, &h1, &h2)
+		if !isGameOver {
+			//Updates
+			UpdatePlayers(&pl1, &pl2)
+			CheckPlayerFloorCollisions(&pl1, &pl2)
+			CheckMovement(&pl1, &pl2)
+			CheckDamage(&pl1, &pl2, &h1, &h2)
 
-		//Rendering
-		rl.BeginDrawing()
+			//Check Game Over
+			if pl1.Health <= 0 || pl2.Health <= 0 {
+				isGameOver = true
+			}
 
-		rl.ClearBackground(rl.Black)
-		rl.DrawRectangle(0, 350, 800, 100, rl.Orange)
-		DrawPlayers(&pl1, &pl2)
-		DrawHealthBars(&h1, &h2)
+			//Rendering
+			rl.BeginDrawing()
 
-		rl.EndDrawing()
+			rl.ClearBackground(rl.Black)
+			rl.DrawRectangle(0, 350, 800, 100, rl.Orange)
+			DrawPlayers(&pl1, &pl2)
+			DrawHealthBars(&h1, &h2)
+
+			rl.EndDrawing()
+		} else {
+
+			rl.BeginDrawing()
+
+			rl.ClearBackground(rl.Red)
+
+			if pl1.Health > 0 {
+				rl.DrawText("Congrats on winning Player 1!", 250, 200, 20, rl.Black)
+			} else if pl2.Health > 0 {
+				rl.DrawText("Congrats on winning Player 2!", 250, 200, 20, rl.Black)
+			}
+			rl.DrawText("Press R to restart the game!", 250, 250, 20, rl.Black)
+
+			if rl.IsKeyPressed(rl.KeyR) {
+				isGameOver = false
+				pl1 = NewPlayer1()
+				pl2 = NewPlayer2()
+				h1 = NewHealthBar1(&pl1)
+				h2 = NewHealthBar2(&pl2)
+			}
+			rl.EndDrawing()
+		}
+
 	}
 }
